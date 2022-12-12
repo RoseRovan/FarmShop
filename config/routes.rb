8e49159d3ad6937cd094d1d607d5c314239a09a0
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2022-12-12 10:57:03 +0300

    defined user methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   resources :products, only: [:index, :show, :update, :destroy, :create]
   # route to test your configuration
-  
+  post '/signup', to: 'users#create' 
+  post '/login', to: 'sessions#create' 
+  delete '/logout', to: 'sessions#destroy'
+  get '/me', to: 'users#show'
 end
